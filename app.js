ac343a99b3b6090d2716570a37c4cbb061fb9395
nomanaziz-xg <noman.aziz@xgrid.co>
2024-04-16 11:16:23 +0500

    retest pipeline

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = 3000
 temp_id = "AKIA6ODU5DHTR5SW6POX"
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
